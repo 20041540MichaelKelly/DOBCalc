04c5906c3a7497b3e45c893889a4aa86175fceeb
Michael Kelly <mikelly1988@gmail.com>
2022-12-01 23:18:30 +0000

    dayThree have implemented the button and more updated UI

--- app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
+++ app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
@@ -1,11 +1,42 @@
 package eu.tutorials.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var tvSelectedDate : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+
+        btnDatePicker.setOnClickListener{
+            onClickDatePicker()
+        }
+    }
+
+    fun onClickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this, { view, selectedYear, selectedMonth, dayOfMonth ->
+            Toast.makeText(this, "Year was $selectedYear, " +
+                    "month was ${selectedMonth +1}, day was $dayOfMonth", Toast.LENGTH_LONG).show()
+
+                       //val selectedDate = "$dayOfMonth/"
+                               }
+            ,year
+            ,month
+            ,day).show()
+
     }
 }
